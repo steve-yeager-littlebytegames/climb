a360a329031a555468af40beeaefb2825f13e1e6
Unknown <steve.yeager@littlebytegames.com>
2018-08-30 23:18:13 -0700

    Flesh out seasons subpages
    Create Seasons.ManageViewModel
    
    
    Create Seasons.SetsViewModel
    
    
    Create Seasons.DataViewModel
    
    
    Correctly show season leave/join buttons
    
    
    Show pending and active seasons in navbar

--- ClimbWeb/Climb/ViewModels/Seasons/HomeViewModel.cs
+++ ClimbWeb/Climb/ViewModels/Seasons/HomeViewModel.cs
@@ -2,53 +2,19 @@
 using System.Linq;
 using Climb.Data;
 using Climb.Models;
-using Microsoft.AspNetCore.Hosting;
 
 namespace Climb.ViewModels.Seasons
 {
-    public class HomeViewModel : BaseViewModel
+    public class HomeViewModel : PageViewModel
     {
-        public Season Season { get; }
-        public int SeasonNumber { get; }
-        public SeasonLeagueUser Participant { get; }
-        public bool CanStartSeason { get; }
         public IEnumerable<SeasonLeagueUser> Participants { get; }
         public IEnumerable<Set> AvailableSets { get; }
 
-        public bool CanLeave => Participant != null && !Season.IsComplete;
-
-        private HomeViewModel(ApplicationUser user, Season season, SeasonLeagueUser participant, bool canStartSeason)
-            : base(user)
+        public HomeViewModel(ApplicationUser user, Season season)
+            : base(user, season)
         {
-            Season = season;
-            Participant = participant;
-            SeasonNumber = season.Index + 1;
-
             Participants = Season.Participants.OrderBy(p => p.Standing);
             AvailableSets = Season.Sets.Where(s => !s.IsComplete).OrderBy(s => s.DueDate);
-
-            CanStartSeason = canStartSeason;
-        }
-
-        public static HomeViewModel Create(ApplicationUser user, Season season, IHostingEnvironment environment)
-        {
-            var participant = user.Seasons
-                .FirstOrDefault(slu => slu.SeasonID == season.ID);
-
-            var canStartSeason = false;
-            if(!season.IsActive && !season.IsComplete)
-            {
-                if(environment.IsDevelopment())
-                {
-                    canStartSeason = true;
-                }
-                else
-                {
-                    canStartSeason = season.League.AdminID == user?.Id;
-                }
-            }
-
-            return new HomeViewModel(user, season, participant, canStartSeason);
         }
     }
 }
