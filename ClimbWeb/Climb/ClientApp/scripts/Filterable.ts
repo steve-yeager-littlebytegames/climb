29e38bd904c21c44f42421911492c529eea258d2
Unknown <steve.yeager@littlebytegames.com>
2018-09-10 20:26:37 -0700

    [ERROR] Get filters working

---
+++ ClimbWeb/Climb/ClientApp/scripts/Filterable.ts
@@ -0,0 +1,52 @@
+﻿export class Filterable {
+    static readonly className = "filterable";
+    static readonly keyAttribute = "data-filter-keys";
+    static readonly filteredClassName = "filtered";
+
+    private readonly element: Element;
+    private readonly keys: string[];
+
+    constructor(element: Element, normalize: boolean) {
+        this.element = element;
+
+        let dataKey = element.getAttribute(Filterable.keyAttribute);
+        if (dataKey) {
+            if (normalize) {
+                dataKey = dataKey.toLowerCase();
+            }
+
+            this.keys = dataKey.split("|");
+        } else {
+            this.keys = new Array<string>();
+        }
+    }
+
+    isValid(filter: string): boolean {
+        return this.keys.find(k => k.includes(filter)) != undefined;
+    }
+
+    tryFilter(filter: string): void {
+        if (this.isValid(filter)) {
+            this.element.classList.remove(Filterable.filteredClassName);
+        } else {
+            this.element.classList.add(Filterable.filteredClassName);
+        }
+    }
+}
+
+export class FilterCollection {
+    private readonly filterables: Filterable[];
+
+    private constructor(filterables: Filterable[]) {
+        this.filterables = filterables;
+    }
+
+    static create(normalize: boolean): FilterCollection {
+        return new FilterCollection(this.collectFilterables(normalize));
+    }
+
+    private static collectFilterables(normalize: boolean): Filterable[] {
+        const filterableElements = Array.from(document.getElementsByClassName(Filterable.className));
+        return filterableElements.map(e => new Filterable(e, normalize));
+    }
+}
