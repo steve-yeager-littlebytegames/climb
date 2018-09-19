30e7a02a18c49ac7a0f00290323c46b54ce96458
Unknown <steve.yeager@littlebytegames.com>
2018-09-18 20:02:10 -0700

    Fix null ref when editing a game fails

--- ClimbWeb/Climb/ViewModels/Games/UpdateviewModel.cs
+++ ClimbWeb/Climb/ViewModels/Games/UpdateviewModel.cs
@@ -10,7 +10,7 @@ namespace Climb.ViewModels.Games
         public Game Game { get; }
         public string Action => Game == null ? "Create" : "Update";
         public string LogoImage { get; }
-        
+        public string PageTitle => Game == null ? "Create Game" : $"Edit {Game.Name}";
 
         public UpdateViewModel(ApplicationUser user, Game game, ICdnService cdnService)
             : base(user)
