7c45356dceeb640d06fb1ac482e0dc032795c1f3
Unknown <steve.yeager@littlebytegames.com>
2018-11-06 19:23:25 -0800

    Create tournament models

---
+++ ClimbWeb/Climb/Models/SetSlot.cs
@@ -0,0 +1,13 @@
+﻿namespace Climb.Models
+{
+    public class SetSlot
+    {
+        public int ID { get; set; }
+        public int TournamentID { get; set; }
+        public string RoundName { get; set; }
+        public int? WinSlotID { get; set; }
+        public int? LoseSlotID { get; set; }
+
+        public Tournament Tournament { get; set; }
+    }
+}
---
+++ ClimbWeb/Climb/Models/Tournament.cs
@@ -0,0 +1,16 @@
+﻿namespace Climb.Models
+{
+    public class Tournament
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int LeagueID { get; set; }
+        public int? SeasonID { get; set; }
+
+        public League League { get; set; }
+        public Season Season { get; set; }
+        public LeagueUser[] LeagueUsers { get; set; }
+        public SetSlot[] SetSlots { get; set; }
+        public Set[] Sets { get; set; }
+    }
+}
