7fff7e43fdbc71ebae244de9a75a932419195fa7
Unknown <steve.yeager@littlebytegames.com>
2018-11-20 23:32:07 -0800

    Add tournaments/competitors page

---
+++ ClimbWeb/Climb/ViewModels/Tournaments/CompetitorsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Climb.Data;
+using Climb.Models;
+
+namespace Climb.ViewModels.Tournaments
+{
+    public class CompetitorsViewModel : PageViewModel
+    {
+        public IReadOnlyList<TournamentUser> Competitors { get; }
+
+        public CompetitorsViewModel(ApplicationUser user, Tournament tournament)
+            : base(user, tournament)
+        {
+            Competitors = tournament.TournamentUsers;
+        }
+    }
+}
