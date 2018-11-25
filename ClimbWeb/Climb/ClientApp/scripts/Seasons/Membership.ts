f4fe0dbab49334f89600275d2301832883ce3cf4
Unknown <steve.yeager@littlebytegames.com>
2018-11-24 20:53:54 -0800

    Organize season typescript files

--- ClimbWeb/Climb/ClientApp/scripts/Seasons/Membership.ts
+++ ClimbWeb/Climb/ClientApp/scripts/Seasons/Membership.ts
@@ -1,9 +1,6 @@
-﻿import { ClimbClient } from "../gen/climbClient.js";
-import * as Filter from "./Filterable.js";
+﻿import { ClimbClient } from "../../gen/climbClient.js";
 
 registerButtons();
-Filter.FilterCollection.create();
-initDescription();
 
 function registerButtons(): void {
     const leaveSeasonButton = document.getElementById("leave-season-button");
@@ -23,20 +20,4 @@ function leaveSeason(participantId: number): void {
             window.location.reload();
         })
         .catch((reason: any) => alert(`Could not leave season.\n${reason}`));
-}
-
-function initDescription(): void {
-    const description = $("#description");
-    const key = "dismissed-description";
-
-    description.on("close.bs.alert", () => window.localStorage.setItem(key, "true"));
-
-    showDescription(description, key);
-}
-
-function showDescription(description: JQuery<HTMLElement>, key: string): void {
-    const hasDismissed = window.localStorage.getItem(key);
-    if (hasDismissed == null) {
-        description.prop("hidden", false);
-    }
 }
