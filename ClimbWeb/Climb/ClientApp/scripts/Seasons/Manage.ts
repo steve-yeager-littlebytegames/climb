28c060ce81f7312d6a96102da0f9fc2e94b87376
Unknown <steve.yeager@littlebytegames.com>
2018-11-24 20:02:20 -0800

    Create tournament from seasons/manage

---
+++ ClimbWeb/Climb/ClientApp/scripts/Seasons/Manage.ts
@@ -0,0 +1,16 @@
+﻿import { ClimbClient } from "../../gen/climbClient.js";
+
+const createTournamentButton = $("#tournament-button");
+createTournamentButton.click(createTournament);
+
+function createTournament() {
+    const tournamentApi = new ClimbClient.TournamentApi();
+    const request = new ClimbClient.CreateRequest();
+    request.leagueID = parseInt(<string>$("#tournament-league-id").val());
+    request.name = <string>$("#tournament-name").val();
+    request.seasonID = parseInt(<string>$("#tournament-season-id").val());
+
+    tournamentApi.post(request)
+        .then(tournament => window.location.href = `/tournaments/home/${tournament.id}`)
+        .catch(() => alert("Could not create tournament."));
+}
