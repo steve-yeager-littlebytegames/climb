a7bbdc866700b0e2638ac4942da879034a676e38
Unknown <steve.yeager@littlebytegames.com>
2018-11-24 20:53:54 -0800

    Organize season typescript files

---
+++ ClimbWeb/Climb/ClientApp/scripts/Seasons/Home.ts
@@ -0,0 +1,20 @@
+﻿import * as Filter from "../Filterable.js";
+
+Filter.FilterCollection.create();
+initDescription();
+
+function initDescription(): void {
+    const description = $("#description");
+    const key = "dismissed-description";
+
+    description.on("close.bs.alert", () => window.localStorage.setItem(key, "true"));
+
+    showDescription(description, key);
+}
+
+function showDescription(description: JQuery<HTMLElement>, key: string): void {
+    const hasDismissed = window.localStorage.getItem(key);
+    if (hasDismissed == null) {
+        description.prop("hidden", false);
+    }
+}
