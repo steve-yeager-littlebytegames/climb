b8b3465a454211f808ed6475d44ed3dcba6833fa
Unknown <steve.yeager@littlebytegames.com>
2018-11-29 19:13:04 -0800

    Setup gulp watch-ts

--- ClimbWeb/Climb/gulpfile.js
+++ ClimbWeb/Climb/gulpfile.js
@@ -1,33 +1,43 @@
 ﻿/// <binding ProjectOpened='watch-less' />
+/// <binding ProjectOpened='watch-ts' />
 const gulp = require("gulp");
 const ts = require("gulp-typescript");
 const less = require("gulp-less");
 const path = require("path");
 
-var tsProject = ts.createProject('tsconfig.gulp.json');
-gulp.task("ts", function () {
-    return gulp.src('ClientApp/scripts/**/*.ts')
-        .pipe(tsProject())
-        .pipe(gulp.dest('wwwroot/dist/scripts'));
-});
+var tsProject = ts.createProject("tsconfig.gulp.json");
+gulp.task("ts",
+    function() {
+        return gulp.src("ClientApp/scripts/**/*.ts")
+            .pipe(tsProject())
+            .pipe(gulp.dest("wwwroot/dist/scripts"));
+    });
 
-var tsClimbProject = ts.createProject('tsconfig.gulp.json');
-gulp.task("ts-climb", function () {
-    return gulp.src('ClientApp/gen/climbClient.ts')
-        .pipe(tsClimbProject())
-        .pipe(gulp.dest('wwwroot/dist/gen'));
-});
+var tsClimbProject = ts.createProject("tsconfig.gulp.json");
+gulp.task("ts-climb",
+    function() {
+        return gulp.src("ClientApp/gen/climbClient.ts")
+            .pipe(tsClimbProject())
+            .pipe(gulp.dest("wwwroot/dist/gen"));
+    });
 
-gulp.task("less", () => {
-    return gulp.src("ClientApp/styles/**/*.less")
-        .pipe(less({
-            paths: [path.join(__dirname, "ClientApp", "styles")]
-        }))
-        .pipe(gulp.dest("wwwroot/dist/styles"));
-});
+gulp.task("less",
+    () => {
+        return gulp.src("ClientApp/styles/**/*.less")
+            .pipe(less({
+                paths: [path.join(__dirname, "ClientApp", "styles")]
+            }))
+            .pipe(gulp.dest("wwwroot/dist/styles"));
+    });
 
-gulp.task("watch-less", () => {
-    gulp.watch("./ClientApp/styles/**/*.less", gulp.series("less"));
-});
+gulp.task("watch-less",
+    () => {
+        gulp.watch("./ClientApp/styles/**/*.less", gulp.series("less"));
+    });
+
+gulp.task("watch-ts",
+    () => {
+        gulp.watch("ClientApp/scripts/**/*.ts", gulp.series("ts"));
+    });
 
 gulp.task("default", gulp.parallel("less", "ts", "ts-climb"));
