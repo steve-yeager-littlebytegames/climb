9554245f2bc9b7a198a6682b14edf1bd658c1937
Unknown <steve.yeager@littlebytegames.com>
2018-12-02 21:38:49 -0800

    Setup class and start API tests

---
+++ ClimbWeb/Climb/Services/Analyzer/IAnalyzerService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Climb.Services
+{
+    public interface IAnalyzerService
+    {
+        Task<AnalyzerDataCollection> Calculate(int player1ID, int player2ID);
+    }
+}
