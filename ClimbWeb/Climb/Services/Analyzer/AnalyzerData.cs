6c315edf60189206428195305696223319688e83
Unknown <steve.yeager@littlebytegames.com>
2018-12-09 22:17:18 -0800

    Send data to WPF app

--- ClimbWeb/Climb/Services/Analyzer/AnalyzerData.cs
+++ ClimbWeb/Climb/Services/Analyzer/AnalyzerData.cs
@@ -1,5 +1,16 @@
 ﻿namespace Climb.Services
 {
+    public class PlayerData<T> : AnalyzerData
+    {
+        public T Player1Data { get; set; }
+        public T Player2Data { get; set; }
+
+        public PlayerData(string name)
+            : base(name)
+        {
+        }
+    }
+
     public abstract class AnalyzerData
     {
         public string Name { get; }
